ad67cd9da0a39a0553f3ddf1039b02668337a7b8
Kevin-133 <20243ds177@utez.edu.mx>
2025-06-13 08:42:17 -0600

    Hice el programa

---
+++ U2_TI_04_Apunte/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module mx.edu.utez.demo1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens mx.edu.utez.demo1 to javafx.fxml;
+    exports mx.edu.utez.demo1;
+}
